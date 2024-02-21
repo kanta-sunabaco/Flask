208fa1030c4235150dfaf596f005373cf4221080
kanta kitani <kanta.76.kame@gmail.com>
2024-02-21 15:57:46 +0900

    app.pyファイルにルーティングを作成

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask
 # Flaskクラスをインスタンス化してapp変数に代入
 app=Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello World!!"
 
-
-
+@app.route("/<name>")
+def name(name):
+    return name+"さんこんにちは"
 
 # スクリプトとして直接実行した場合
 if __name__=="__main__":
